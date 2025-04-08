d848a6649e2a39ab5d5635967aa0a0bdec23cd75
FardinMahadi <mahadihasanfardin2015@gmail.com>
2025-04-08 09:57:45 +0600

    admin stas added

--- index.js
+++ index.js
@@ -279,6 +279,35 @@ async function run() {
       res.send({ paymentResult, deleteResult });
     });
 
+    // stats or analytics
+    app.get("/admin-stats", verifyToken, verifyAdmin, async (req, res) => {
+      const users = await userCollection.estimatedDocumentCount();
+      const menuItems = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+
+      // Use aggregation to calculate total revenue
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $group: {
+              _id: null,
+              totalRevenue: {
+                $sum: "$price",
+              },
+            },
+          },
+        ])
+        .toArray();
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+
+      res.send({
+        users,
+        menuItems,
+        orders,
+        revenue,
+      });
+    });
+
     // MongoDB Connection Test
     await client.db("admin").command({ ping: 1 });
     console.log(
